c507b6ec4938f533e5cf0bcb51210b90be4446db
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2014-06-18 22:46:14 -0500

    updated observer

--- labs/observer/trunk/src/TwitterListener.java
+++ labs/observer/trunk/src/TwitterListener.java
@@ -7,57 +7,70 @@ import java.util.Map.Entry;
 
 public class TwitterListener implements StatusListener, Subject {
 
-  private Map<Observer, Set<String>> mapObservers
-    = new HashMap<Observer, Set<String>>();
+    private Map<Observer, Set<String>> mapObservers
+            = new HashMap<Observer, Set<String>>();
 
-  @Override 
-  public void onStatus(Status status) {
-    // you need to write some codes here 
-  }
+    @Override
+    public void onStatus(twitter4j.Status status) {
+        notifyObservers(status.getText());
+    }
 
-  @Override
-  public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-    System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-  }
+    @Override
+    public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+        System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+    }
 
-  @Override
-  public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-    System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-  }
+    @Override
+    public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+        System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+    }
 
-  @Override
-  public void onScrubGeo(long userId, long upToStatusId) {
-    System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-  }
+    @Override
+    public void onScrubGeo(long userId, long upToStatusId) {
+        System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+    }
 
-  @Override
-  public void onStallWarning(StallWarning warning) {
-    System.out.println("Got stall warning:" + warning);
-  }
+    @Override
+    public void onStallWarning(StallWarning warning) {
+        System.out.println("Got stall warning:" + warning);
+    }
 
-  @Override
-  public void onException(Exception ex) {
-    ex.printStackTrace();
-  }
+    @Override
+    public void onException(Exception ex) {
+        ex.printStackTrace();
+    }
 
-  @Override // implementing method defined in Subject interface
-  public void notifyObservers(String text) {
-    // you need to write some codes here 
-  }
+    @Override // implementing method defined in Subject interface
+    public void notifyObservers(String text) {
+        for (Observer o : mapObservers.keySet()){
+            o.update(text);
+        }
+    }
 
-  @Override // implementing method defined in Subject interface
-  public boolean removeObserver(Observer observer) {
-    boolean result = false;
-    // you need to write some codes here 
+    @Override // implementing method defined in Subject interface
+    public boolean removeObserver(Observer observer) {
+        boolean result = false;
 
-    return result;
-  }
+        if (mapObservers.containsKey(observer)){
+            mapObservers.remove(observer);
+            result = true;
+        }
 
-  @Override  // implementing method defined in Subject interface
-  public boolean registerObserver(Observer observer, String track) {
-    boolean result = false;
-    // you need to write some codes here 
+        return result;
+    }
 
-    return result;
-  }
+    @Override  // implementing method defined in Subject interface
+    public boolean registerObserver(Observer observer, String track) {
+        boolean result = false;
+
+        Set<String> set = new HashSet<>();
+        set.add(track);
+
+        if (!mapObservers.containsKey(observer)){
+            mapObservers.put(observer, set);
+            result = true;
+        }
+
+        return result;
+    }
 }
