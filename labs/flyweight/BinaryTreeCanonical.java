fa5f39051241c443df193d76a4c732cd29a91856
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2014-07-11 13:47:35 -0500

    updated flyweight lab

--- labs/flyweight/BinaryTreeCanonical.java
+++ labs/flyweight/BinaryTreeCanonical.java
@@ -1,30 +1,69 @@
-import java.util.Set;
-import java.util.HashSet;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BinaryTreeCanonical {
+    private static Map<Integer, BinaryTreeNode> nodeMap = new HashMap<>();
 
-  static class BinaryTreeNode {
-    int key;
-    BinaryTreeNode left, right;
-    Integer cachedHash; // can speed up hash function computation hugely
+    static class BinaryTreeNode {
+        int key;
+        BinaryTreeNode left, right;
+        Integer cachedHash; // can speed up hash function computation hugely
 
-    public BinaryTreeNode(int k, BinaryTreeNode l, BinaryTreeNode r) {
-      this.key = k;
-      this.left = l;
-      this.right = r;
-      this.cachedHash = null;
+        public BinaryTreeNode(int k, BinaryTreeNode l, BinaryTreeNode r) {
+            this.cachedHash = -1; // sets cachedHash to a value that will not be in nodeMap
+            this.key = k;
+            this.left = l;
+            this.right = r;
+
+            getCanonical(this); // sets cachedHash for this instance
+        }
+
+        @Override
+        public int hashCode(){
+            return cachedHash;
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            try {
+                BinaryTreeNode node = (BinaryTreeNode)obj;
+                return node.key == this.key
+                        && node.left == this.left
+                        && node.right == this.right;
+            }
+            catch (Exception ex) {
+                return false;
+            }
+        }
     }
-  }
 
-  static BinaryTreeNode getCanonical(BinaryTreeNode n) {
-    return null;
-  } 
+    static BinaryTreeNode getCanonical(BinaryTreeNode n) {
+        // if the cachedHash is greater than -1 we can use it to get the node from nodeMap
+        if (n.cachedHash >= 0 && nodeMap.containsValue(n)){
+            n = nodeMap.get(n.cachedHash);
+        }
+        else if (nodeMap.containsValue(n)){
+            // when n's cachedHash has not been set we can try to find an instance in nodeMap
+            // that matches n and set n equal to the nodeMap instance
 
-  static int numberOfFlyweightNodes() {
-    return -1;
-  }
+            for (Map.Entry<Integer, BinaryTreeNode> set : nodeMap.entrySet()){
+                if (set.getValue().equals(n)){
+                    n = set.getValue();
+                    break;
+                }
+            }
+        }
+        else {
+            // set the cachedHash for n and add it to the nodeMap
+
+            n.cachedHash = nodeMap.size();
+            nodeMap.put(n.cachedHash, n);
+        }
+
+        return n;
+    }
+
+    static int numberOfFlyweightNodes() {
+        return nodeMap.size();
+    }
 }
