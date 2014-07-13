d746908518cd0c924f3bc6aecaa296ff1476c1b1
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2014-07-13 14:32:40 -0500

    added tests to iterator lab to check for exceptions

--- labs/iterator/TestCustomIterator.java
+++ labs/iterator/TestCustomIterator.java
@@ -1,47 +1,78 @@
 import org.junit.*;
+
 import static org.junit.Assert.*;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class TestCustomIterator{
+public class TestCustomIterator {
 
-  @Before
-  public void setUp() {
-  }
+    @Before
+    public void setUp() {
+    }
 
-  @Test(timeout=1000)
-  public void testStudentIterator() {
-    Student [] testCase = Student.createTestArray();
-    Iterator<Student> berkeleyIterator = 
-        new CustomIterator.StudentIteratorBySchool(testCase, "Berkeley");
-    assertEquals(berkeleyIterator.next().toString(), "Aardvark Smith,3.5,18,Berkeley");
-    assertEquals(berkeleyIterator.next().toString(), "Matt Biondi,3.3,22,Berkeley");
-    assertFalse(berkeleyIterator.hasNext() );
+    @Test(timeout = 1000)
+    public void testStudentIterator() {
+        Student[] testCase = Student.createTestArray();
+        Iterator<Student> berkeleyIterator =
+                new CustomIterator.StudentIteratorBySchool(testCase, "Berkeley");
+        assertEquals(berkeleyIterator.next().toString(), "Aardvark Smith,3.5,18,Berkeley");
+        assertEquals(berkeleyIterator.next().toString(), "Matt Biondi,3.3,22,Berkeley");
+        assertFalse(berkeleyIterator.hasNext());
 
-    Iterator<Student> mitIterator = 
-        new CustomIterator.StudentIteratorBySchool(testCase, "MIT");
-    assertEquals(mitIterator.next().toString(), "Imran Aziz,3.6,20,MIT");
-    assertFalse(mitIterator.hasNext() );
+        Iterator<Student> mitIterator =
+                new CustomIterator.StudentIteratorBySchool(testCase, "MIT");
+        assertEquals(mitIterator.next().toString(), "Imran Aziz,3.6,20,MIT");
+        assertFalse(mitIterator.hasNext());
 
-    Iterator<Student> stanfordIterator = 
-        new CustomIterator.StudentIteratorBySchool(testCase, "Stanford");
-    assertFalse(stanfordIterator.hasNext() );
-  }
+        Iterator<Student> stanfordIterator =
+                new CustomIterator.StudentIteratorBySchool(testCase, "Stanford");
+        assertFalse(stanfordIterator.hasNext());
+    }
 
-  @Test(timeout=1000)
-  public void testPredicatedIterator() {
-    Student[] testCase = Student.createTestArray();
-    Iterator<Student> berkeleyIterator = 
-      new CustomIterator.StudentIteratorPredicated(
-        new CustomIterator.StudentPredicate() {
-          public boolean check(Student student) {
-	    return (student.school == "Berkeley");
-	  }
-	}, testCase);
+    @Test(timeout = 1000)
+    public void testPredicatedIterator() {
+        Student[] testCase = Student.createTestArray();
+        Iterator<Student> berkeleyIterator =
+                new CustomIterator.StudentIteratorPredicated(
+                        new CustomIterator.StudentPredicate() {
+                            public boolean check(Student student) {
+                                return (student.school == "Berkeley");
+                            }
+                        }, testCase);
 
-   assertEquals(berkeleyIterator.next().toString(), "Aardvark Smith,3.5,18,Berkeley");
-   assertEquals(berkeleyIterator.next().toString(), "Matt Biondi,3.3,22,Berkeley");
-   assertFalse(berkeleyIterator.hasNext());
+        assertEquals(berkeleyIterator.next().toString(), "Aardvark Smith,3.5,18,Berkeley");
+        assertEquals(berkeleyIterator.next().toString(), "Matt Biondi,3.3,22,Berkeley");
+        assertFalse(berkeleyIterator.hasNext());
+    }
 
-    
-  }
+    @Test(timeout=1000, expected = NoSuchElementException.class)
+    public void testShouldThrowNoSuchElementException(){
+        Student[] testCase = Student.createTestArray();
+        Iterator<Student> berkeleyIterator =
+                new CustomIterator.StudentIteratorPredicated(
+                        new CustomIterator.StudentPredicate() {
+                            public boolean check(Student student) {
+                                return (student.school == "Berkeley");
+                            }
+                        }, testCase);
+
+        assertEquals(berkeleyIterator.next().toString(), "Aardvark Smith,3.5,18,Berkeley");
+        assertEquals(berkeleyIterator.next().toString(), "Matt Biondi,3.3,22,Berkeley");
+        berkeleyIterator.next();
+    }
+
+    @Test(timeout=1000, expected = UnsupportedOperationException.class)
+    public void testShouldThrowUnsupportedOperationException(){
+        Student[] testCase = Student.createTestArray();
+        Iterator<Student> berkeleyIterator =
+                new CustomIterator.StudentIteratorPredicated(
+                        new CustomIterator.StudentPredicate() {
+                            public boolean check(Student student) {
+                                return (student.school == "Berkeley");
+                            }
+                        }, testCase);
+
+        berkeleyIterator.remove();
+    }
 }
